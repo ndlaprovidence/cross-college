c203b47d122173e7f6c864469384252f162d1412
GENISSEL Benjamin <geniben@ndlp.fr>
2022-12-08 16:13:56 +0100

    Race Import And Students Crud

--- src/Controller/RankingController.php
+++ src/Controller/RankingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RankingRepository;
 use PDO;
 use PDOException;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,25 +11,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RankingController extends AbstractController
 {
+    /*
     #[Route('/ranking', name: 'app_ranking')]
     public function index(): Response
     {
-        $serveur = "localhost";
-        $dbname = "kidrun";
-        $user = "root";
-        $pass = "root";
-        
+        $error_message = "";
         $rows = array();
-        $error_message = "";        
+
+        $dbserver = $this->getParameter("dbserver");
+        $dbport = $this->getParameter("dbport");
+        $dbname = $this->getParameter("dbname");
+        $dbuser = $this->getParameter("dbuser");
+        $dbpassword = $this->getParameter("dbpassword");        
 
         try{
-            $connexion = new PDO("mysql:host=$serveur;port=3307;dbname=$dbname",$user,$pass);
+            $connexion = new PDO("mysql:host=$dbserver;port=$dbport;dbname=$dbname", $dbuser, $dbpassword);
+            
             $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            $sth = $connexion->prepare("SELECT * from eleve");
+            $sth = $connexion->prepare("SELECT * from tbl_student");
             $sth->execute();
 
-            /* Récupération de toutes les lignes d'un jeu de résultats */
             //print("Récupération de toutes les lignes d'un jeu de résultats :\n");
             $rows = $sth->fetchAll();
             //dump($rows);
@@ -39,9 +42,28 @@ class RankingController extends AbstractController
         }
 
 
+
         return $this->render('ranking/index.html.twig', [
             'rows' => $rows,
             'error_message' => $error_message,
         ]);
     }
+    */
+
+    #[Route('/ranking', name: 'app_ranking')]
+    public function index(RankingRepository $rankingRepository): Response
+    {
+        $error_message = "";
+        $rows = array();
+
+        $rows = $rankingRepository->findAll();
+
+
+
+        return $this->render('ranking/index.html.twig', [
+            'rows' => $rows,
+            'error_message' => $error_message,
+        ]);
+    }
+
 }
