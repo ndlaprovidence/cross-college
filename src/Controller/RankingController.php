7eda28def148ce19d4fda43f08b8d5ea823f5f28
KURANDY Eva <kuraeva@ndlp.fr>
2022-12-15 17:04:33 +0100

    classement

--- src/Controller/RankingController.php
+++ src/Controller/RankingController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use App\Repository\RankingRepository;
 use PDO;
 use PDOException;
+use App\Repository\GradeRepository;
+use App\Repository\RankingRepository;
+use App\Repository\StudentRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,51 +17,49 @@ class RankingController extends AbstractController
     #[Route('/ranking', name: 'app_ranking')]
     public function index(): Response
     {
-        $error_message = "";
-        $rows = array();
-
-        $dbserver = $this->getParameter("dbserver");
-        $dbport = $this->getParameter("dbport");
-        $dbname = $this->getParameter("dbname");
-        $dbuser = $this->getParameter("dbuser");
-        $dbpassword = $this->getParameter("dbpassword");        
-
-        try{
-            $connexion = new PDO("mysql:host=$dbserver;port=$dbport;dbname=$dbname", $dbuser, $dbpassword);
-            
-            $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            $sth = $connexion->prepare("SELECT * from tbl_student");
-            $sth->execute();
-
-            //print("Récupération de toutes les lignes d'un jeu de résultats :\n");
-            $rows = $sth->fetchAll();
-            //dump($rows);
-
-        }
-        catch(PDOException $e){
-            $error_message = $e->getMessage();
-        }
-
-
-
-        return $this->render('ranking/index.html.twig', [
-            'rows' => $rows,
-            'error_message' => $error_message,
-        ]);
+    $error_message = "";
+    $rows = array();
+    $dbserver = $this->getParameter("dbserver");
+    $dbport = $this->getParameter("dbport");
+    $dbname = $this->getParameter("dbname");
+    $dbuser = $this->getParameter("dbuser");
+    $dbpassword = $this->getParameter("dbpassword");        
+    try{
+    $connexion = new PDO("mysql:host=$dbserver;port=$dbport;dbname=$dbname", $dbuser, $dbpassword);
+    
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sth = $connexion->prepare("SELECT * from tbl_student");
+    $sth->execute();
+    //print("Récupération de toutes les lignes d'un jeu de résultats :\n");
+    $rows = $sth->fetchAll();
+    //dump($rows);
+    }
+    catch(PDOException $e){
+    $error_message = $e->getMessage();
+    }
+    return $this->render('ranking/index.html.twig', [
+    'rows' => $rows,
+    'error_message' => $error_message,
+    ]);
     }
     */
 
     #[Route('/ranking', name: 'app_ranking')]
-    public function index(RankingRepository $rankingRepository): Response
+    public function index(RankingRepository $rankingRepository, GradeRepository $gradeRepository, StudentRepository $studentRepository): Response
     {
         $error_message = "";
         $rows = array();
 
-        $rows = $rankingRepository->findAll();
+        $rows = $rankingRepository->findStudentsWithGrades();
+        $grades = $gradeRepository->findAll();
+        $students = $studentRepository->findAll();
 
         return $this->render('ranking/index.html.twig', [
             'rows' => $rows,
+            'grades' => $grades,
+            'students' => $students,
+            'levels' => array(6, 5, 4, 3),
+            'genders' => array('F', 'G'),
             'error_message' => $error_message,
         ]);
     }
