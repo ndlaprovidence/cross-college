a6cec3de90dc7e360198a038c9723ddc066117e6
Kozen1 <benji6127@hotmail.fr>
2023-03-06 10:52:28 +0100

    first commit

--- src/Controller/Ranking2Controller.php
+++ src/Controller/Ranking2Controller.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use PhpSerial;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,53 +15,39 @@ class Ranking2Controller extends AbstractController
     /**
      * @Route("/ranking2", name="ranking2")
      */
-    public function index(Request $request)
+    public function createRanking2Action(Request $request)
     {
+        $barcode = "";
+        // Création du formulaire
         $form = $this->createFormBuilder()
-            ->add('barcode', TextType::class, ['required' => true])
+            ->add('barcode', TextType::class, [
+                'label' => 'Barcode',
+                'attr' => [
+                    'readonly' => false,
+                ],
+                'constraints' => [
+                    new Length(['min' => 3, 'max' => 4])
+                ],
+            ])
             ->add('submit', SubmitType::class, ['label' => 'Envoyer'])
             ->getForm();
 
+        // Traitement du formulaire
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // Le formulaire a été envoyé, faites ce que vous voulez avec le code-barres ici
-            $barcode = $form->getData()['barcode'];
-            //...
+            $data = $form->getData();
+            $barcode = $data['barcode'];        
+            // Do something with the submitted data, e.g. save it to a database
+
+            // Redirect to a success page
+            // return $this->redirectToRoute('/ranking2');
         }
 
-        // Configuration de PHP Serial
-        $serial = new PhpSerial;
-        $serial->deviceSet('/dev/ttyUSB0'); // Remplacez ttyUSB0 par le port série approprié pour votre douchette
-        $serial->confBaudRate(9600); // Configurez la vitesse de transmission appropriée pour votre douchette
-        $serial->confParity('none');
-        $serial->confCharacterLength(8);
-        $serial->confStopBits(1);
-        $serial->confFlowControl('none');
-        $serial->deviceOpen();
-
-        // Lecture en boucle des codes-barres
-        while (true) {
-            $barcode = '';
-            while (true) {
-                $char = $serial->readPort(1);
-                if ($char == "\n") { // Le code-barres se termine généralement par un caractère de retour à la ligne
-                    break;
-                } else {
-                    $barcode .= $char;
-                }
-            }
-            $form = $this->createFormBuilder()
-                ->add('barcode', TextType::class, ['required' => true, 'data' => $barcode])
-                ->add('submit', SubmitType::class, ['label' => 'Envoyer'])
-                ->getForm();
-            break; // Sort de la boucle de lecture après avoir lu un code-barres
-        }
-
-        $serial->deviceClose();
-
+        // Affichage du formulaire
         return $this->render('ranking2/index.html.twig', [
             'form' => $form->createView(),
+            'barcode' => $barcode,
         ]);
     }
 }
