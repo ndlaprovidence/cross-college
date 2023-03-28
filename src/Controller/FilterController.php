3ebdfd56ae6e6fde960c0ed678d8384bc242c8d8
Kozen1 <benji6127@hotmail.fr>
2023-03-28 15:01:41 +0200

    Démo n°3

--- src/Controller/FilterController.php
+++ src/Controller/FilterController.php
@@ -10,9 +10,13 @@ use App\Repository\GradeRepository;
 use App\Repository\FilterRepository;
 use App\Repository\RankingRepository;
 use App\Repository\StudentRepository;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FilterController extends AbstractController
@@ -83,5 +87,52 @@ class FilterController extends AbstractController
             'gender_checked' => $gender,
             'error_message' => $error_message,
         ]);
+    }
+
+    #[Route('/export', name: 'app_export')]
+    public function export(Request $request, FilterRepository $filterRepository)
+    {
+        // TODO: exporter le tableau filtré au format Excel
+        $grade = $request->query->get('grades');
+        $level = $request->query->get('levels');
+        $gender = $request->query->get('genders');
+
+        $rows = $filterRepository->findStudentsWithGrades($grade, $level, $gender);
+
+        // créer un nouveau classeur
+        $spreadsheet = new Spreadsheet();
+
+        // sélectionner la feuille active
+        $sheet = $spreadsheet->getActiveSheet();
+
+        // ajouter les en-têtes de colonne
+        $sheet->setCellValue('A1', 'Classement');
+        $sheet->setCellValue('B1', 'Elève');
+        $sheet->setCellValue('C1', 'Classe');
+        $sheet->setCellValue('D1', 'Catégorie');
+        $sheet->setCellValue('E1', 'Genre');
+        $sheet->setCellValue('F1', 'Chronomètre');
+
+        // ajouter les données
+        $i = 2;
+    foreach ($rows as $row) {
+        $sheet->setCellValue('A' . $i, $row['classement']);
+        $sheet->setCellValue('B' . $i, $row['firstname'] . ' ' . $row['lastname']);
+        $sheet->setCellValue('C' . $i, $row['shortname']);
+        $sheet->setCellValue('D' . $i, $row['level']);
+        $sheet->setCellValue('E' . $i, $row['gender']);
+        $sheet->setCellValue('F' . $i, $row['chronometre']->format('H:i:s'));
+        $i++;
+    }
+
+    // générer le fichier Excel
+    $writer = new Xlsx($spreadsheet);
+    $writer->save('export.xlsx');
+
+    // envoyer le fichier Excel en réponse à la requête
+    $response = new BinaryFileResponse('export.xlsx');
+    $response->headers->set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'export.xlsx');
+    return $response;
     }
 }
